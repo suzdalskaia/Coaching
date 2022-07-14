2f2c17931b9711183493ec072c174e20edef6fc7
Suzdalskaia Elizaveta <e.suzdalskaia@coworking.local>
2022-07-14 19:52:00 +0300

    Add Indian code

---
+++ Lab1-problem-108/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Lab1_problem_108
+{
+    class Program
+    {
+        //задача вивести середнє число з 3х цілих, які по модулю менше ніж 1000
+
+        static void Main(string[] args)
+        {
+            int a = 11;
+            int b = 3;
+            int c = 7;
+
+            int medium;
+
+            if(a >=-1000 && a <= 1000 && b >= -1000 && b <= 1000 && c >= -1000 && c <= 1000)
+            {
+                if (a < b)
+                {
+                    if (b < c)
+                    {
+                        medium = b;
+                    }
+                    else
+                    {
+                        if (a < c)
+                        {
+                            medium = c;
+                        }
+                        else
+                        {
+                            medium = a;
+                        }
+                    }
+                }
+                else
+                {
+                    if (c < b)
+                    {
+                        medium = b;
+                    }
+                    else
+                    {
+                        if (c < a)
+                        {
+                            medium = c;
+                        }
+                        else
+                        {
+                            medium = a;
+                        }
+                    }
+                }
+
+                Console.WriteLine(medium);
+                Console.ReadKey();
+            }
+        }
+    }
+}
