ba6a3a39690154ec3c03b0814355f3ad593ea7b5
Suzdalskaia Elizaveta <e.suzdalskaia@room8studio.com>
2022-08-05 15:10:19 +0300

    letters to numbers

---
+++ Letters-To-Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LettersToNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Type a message");
+            string input = Console.ReadLine();
+            string output = null;
+
+            if (input != null)
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    int index = char.ToUpper(input[i]) - 64;
+                    if (!(index >= 1 && index <= 26))
+                    {
+                        continue;
+                    }
+                    output += $"{index} ";
+                }
+            }
+
+            if (output != null)
+            {
+                Console.WriteLine(output);
+            }
+            else
+            {
+                Console.WriteLine("Please enter your message in english only");
+            }
+
+        }
+    }
+}
